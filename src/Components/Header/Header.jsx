43fd048d2a5519c77a1ab3ae97a6dd95b079faac
enriktigasna <maxim.pavlenko2011@gmail.com>
2024-02-06 20:23:05 +0100

    Added responsiveness to hamburger menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -12,22 +12,22 @@ export default function Header() {
 
   return (
     <>
-    <div>{navOpen ? <Hamburger setOpen={setNavOpen} /> : null}</div>
-    <div className="h-[56px] bg-dark px-5 py-[3px] flex flex-col justify-center content-center">
-      <div className="flex justify-between w-[100%]">
-      <MdMenu onClick={() => { setNavOpen(!navOpen);}} className="h-[100%] w-6 text-gold"/>
-        <div className="text-gold place-self-center  text-center">LOGO</div>
-        <div className="flex gap-4">
-          <MdOutlineCast className="h-[100%] w-6" />
-          <Image
-            className="rounded-full"
-            src={pfp}
-            alt="Profile Picture"
-            width={38}
-          />
+      {navOpen ? <Hamburger setOpen={setNavOpen} /> : null}
+      <div className="h-[56px] bg-dark px-5 py-[3px] flex flex-col justify-center content-center">
+        <div className="flex justify-between w-[100%]">
+        <MdMenu onClick={() => { setNavOpen(!navOpen);}} className="h-[100%] w-6 text-gold"/>
+          <div className="text-gold place-self-center  text-center">LOGO</div>
+          <div className="flex gap-4">
+            <MdOutlineCast className="h-[100%] w-6" />
+            <Image
+              className="rounded-full"
+              src={pfp}
+              alt="Profile Picture"
+              width={38}
+            />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
