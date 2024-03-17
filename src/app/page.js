988f4e83d044bbfdb592059e38c70a6de08c6999
gyuni1337 <silviplosakov@gmail.com>
2024-03-17 17:24:30 +0100

    began implementing the back-end, some research about route handlers and some structure planing

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 import Search from "@/Components/Inputs/Search/Search";
 import GenreFeed from "@/Components/Genre/GenreFeed/GenreFeed";
-import { DataProvider } from '../DataContext/DataContext'; // Adjust the path as needed
+import { DataProvider } from '../DataContext/Datacontext';
 
 export default function Home() {
   return (
