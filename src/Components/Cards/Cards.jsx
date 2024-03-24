4292c1cc9c745f1f5204be320c0d63f777c07212
gyuni1337 <silviplosakov@gmail.com>
2024-03-24 22:52:55 +0100

    -fawaz eh :||||||

---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import './Index.css';
+
+
+
+function Cards() {
+    return (
+
+            <>
+            
+            
+            
+            </>
+        // <div className={"container"}>
+        //     <h1 className="text-white text-2xl justify-start ml-20 font-bold p-2">Favorites</h1>
+
+
+        //     <div className={"carousel"}>
+        //         <div className={"carouselbox"}>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+
+        //         </div>
+        //         <a className={"switchLeft sliderButton"}></a>
+        //         <a className={"switchRight sliderButton"}></a>
+        //     </div>
+
+        //     <h1 className="text-white text-2xl justify-start ml-20 font-bold p-2">Recently Added</h1>
+
+
+        //     <div className={"carousel"}>
+        //         <div className={"carouselbox"}>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+
+        //         </div>
+        //         <a className={"switchLeft sliderButton"}></a>
+        //         <a className={"switchRight sliderButton"}></a>
+        //     </div>
+        //     <h1 className="text-white text-2xl justify-start ml-20 font-bold p-2">Movies</h1>
+
+
+        //     <div className={"carousel"}>
+        //         <div className={"carouselbox"}>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+
+        //         </div>
+        //         <a className={"switchLeft sliderButton"}></a>
+        //         <a className={"switchRight sliderButton"}></a>
+        //     </div>
+
+        //     <h1 className="text-white text-2xl justify-start ml-20 font-bold p-2">Series</h1>
+
+
+        //     <div className={"carousel"}>
+        //         <div className={"carouselbox"}>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+        //             <img src={"https://placehold.co/600x400?text=Hello\\nWorld"} className={""}></img>
+
+        //         </div>
+        //         <a className={"switchLeft sliderButton"}></a>
+        //         <a className={"switchRight sliderButton"}></a>
+        //     </div>
+        // </div>
+    )
+}
+
+export default Cards;
