9100df8da07fc042567c80e15fe91c73fc406ff1
gyuni1337 <silviplosakov@gmail.com>
2024-04-11 19:31:07 +0200

    fixing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client';
 
 import Footer from "../Components/Footer/Footer";
@@ -10,7 +9,7 @@ import { firestore, auth } from "../app/lib/firebase";
 import { useEffect } from "react";
 import React from 'react';
 import VideoPlayer from '@/Components/VideoDownload/Video'; // Adjust the path as necessary
-
+import Cards from "@/Components/Cards/Cards";
 export default function Home() {
 
   useEffect(() => {
@@ -26,34 +25,12 @@ export default function Home() {
         <div className="flex flex-col min-h-screen">
           <Header />
           <Search />
-          <VideoPlayer src="path/to/your/video.mp4" /> {/* Add the VideoPlayer component */}
+          <VideoPlayer src="path/to/your/video.mp4" /> 
           <GenreFeed />
           <Footer />
         </div>
       </DataProvider>
     </>
   );
-=======
-import Footer from "@/components/Footer/Footer";
-import Header from "@/components/Header/Header";
-import Search from "@/Components/Inputs/Search/Search";
-import GenreFeed from "@/Components/Genre/GenreFeed/GenreFeed";
-import { DataProvider } from '../DataContext/DataContext'; // Adjust the path as needed
-import Cards from "@/Components/Cards/Cards";
-export default function Home() {
-    return (
-        <>
-            <DataProvider>
-                <div className="flex flex-col min-h-screen">
-                    <Header />
-                    <Search />
-                    <GenreFeed />
-                    <Cards title={"Main movie idk"} vadfuckduvill={"random bror"} serie={"Serie - 2019"}/>
-                    <Footer />
-                </div>
-            </DataProvider>
-        </>
-    );
->>>>>>> Fawaz
 }
 
